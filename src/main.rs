b062eef6280ea25cac1d0d4ee66fc49b1e8cd317
Bart Massey <bart@cs.pdx.edu>
2016-07-18 02:19:46 -0700

    added wait for child

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate libc;
 
 use std::io::prelude::*;
 use std::fs::OpenOptions;
-use std::io::{Error, stderr, BufReader};
+use std::io::{Error, BufReader};
 
 // Borrowed from https://github.com/stemjail/tty-rs
 mod pty {
@@ -11,6 +11,8 @@ mod pty {
     use std::os::unix::io::AsRawFd;
     use std::ffi::{CStr, OsString};
     use std::os::unix::ffi::OsStringExt;
+    use libc::{c_int, pid_t};
+    use libc::waitpid as raw_waitpid;
 
     mod raw {
         use libc::{c_int, c_char};
@@ -47,23 +49,28 @@ mod pty {
         let os_string = OsString::from_vec(buf.as_bytes().to_vec());
         Ok(PathBuf::from(os_string))
     }
+
+    pub fn waitpid(pid: pid_t) -> Result<i32> {
+        let mut status: c_int = 0;
+        match unsafe { raw_waitpid(pid, &mut status as *mut c_int, 0) } {
+            -1 => Err(Error::last_os_error()),
+            _ => Ok(status as i32)
+        }
+    }
 }
 
 pub fn main() {
     let mut master = OpenOptions::new()
                     .read(true).write(true)
                     .open("/dev/ptmx").expect("cannot open ptmx");
-    
     pty::grantpt(&mut master).expect("could not grant pty");
     pty::unlockpt(&mut master).expect("could not unlock pty");
-
     let pid = unsafe{ libc::fork() };
     match pid {
         -1 => {
             panic!("fork failed: {}", Error::last_os_error());
         },
         0 => {
-            stderr().write("slave starting!\n".as_bytes()).expect("oops");
             let slave_name = pty::ptsname(&mut master)
                              .expect("cannot get pty name");
             drop(master);
@@ -78,19 +85,14 @@ pub fn main() {
             slave.write("hello world\n".as_bytes())
                  .expect("cannot write to /dev/pty");
             slave.flush().expect("cannot flush /dev/pty");
-            drop(slave);
-            stderr().write("slave sleeping!\n".as_bytes()).expect("oops");
-            std::thread::sleep(std::time::Duration::from_secs(1));
-            stderr().write("slave exiting!\n".as_bytes()).expect("oops");
         },
         _ => {
-
-            println!("pid: {}", pid);
             let mut master_buf = BufReader::new(&master);
             let mut message = String::new();
             master_buf.read_line(&mut message)
                       .expect("could not read message");
-            print!("received message: {}", message);
+            assert!(message.trim() == "hello world");
+            assert_eq!(pty::waitpid(pid).expect("child exited abnormally"), 0);
         }
     }
 }
