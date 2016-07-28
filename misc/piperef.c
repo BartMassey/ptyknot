07516b9c66341d8480ed5d1c46be568eeb1e65ae
Bart Massey <bart@cs.pdx.edu>
2016-07-27 22:32:07 -0700

    updated ptyref.c; added piperef.c

--- misc/piperef.c
+++ misc/piperef.c
@@ -1,8 +1,7 @@
 /* Copyright © 2016 Bart Massey */
 
-/* Demonstrate reading from child controlling terminal. */
+/* Demonstrate pipes and redirecting child stdout. */
 
-#define _XOPEN_SOURCE
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,36 +11,29 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <string.h>
-#include <fcntl.h>
 
 static char strbuf[1024];
 
 int main(void) {
-    int ptmx = open("/dev/ptmx", O_RDONLY);
-    assert(ptmx >= 0);
-    assert(grantpt(ptmx) == 0);
-    assert(unlockpt(ptmx) == 0);
+    int pipefds[2];
+    assert(pipe(pipefds) == 0);
     pid_t pid = fork();
     assert(pid >= 0);
     if (pid == 0) {
+        assert(close(pipefds[0]) == 0);
         int spid = setsid();
         if (spid == -1) {
             perror("setsid");
             abort();
         }
-        char *pts = ptsname(ptmx);
-        assert(pts);
-        int pty = open(pts, O_RDWR, 0);
-        assert(pty >= 0);
-        assert(close(pty) == 0);
-        int tty = open("/dev/tty", O_RDWR, 0);
-        FILE *ttyf = fdopen(tty, "r+");
-        assert (ttyf);
-        fprintf(ttyf, "hello world\n");
+        assert(dup2(pipefds[1], 1) == 1);
+        printf("hello world\n");
         exit(0);
     }
-    FILE *ptmxf = fdopen(ptmx, "r");
-    fgets(strbuf, sizeof(strbuf), ptmxf);
+    assert(close(pipefds[1]) == 0);
+    FILE *child_stdout = fdopen(pipefds[0], "r");
+    assert(child_stdout);
+    fgets(strbuf, sizeof(strbuf), child_stdout);
     fprintf(stderr, "got %s", strbuf);
     int wstatus = 0;
     pid_t result = waitpid(pid, &wstatus, 0);
