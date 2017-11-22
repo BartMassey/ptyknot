c84b5a40f2d57c2a4a9ba5333d45f3c30719a9f2
Bart Massey <bart@cs.pdx.edu>
2017-11-21 21:07:05 -0800

    another try at gh pages

---
+++ docs/implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ptyknot"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"ptyknot/struct.PtyKnot.html\" title=\"struct ptyknot::PtyKnot\">PtyKnot</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
