db106bef00284bfbb141292210458c6543f20966
Bart Massey <bart@cs.pdx.edu>
2018-10-30 12:16:09 -0700

    updated docs

--- target/doc/implementors/core/ops/drop/trait.Drop.js
+++ target/doc/implementors/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["ptyknot"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"ptyknot/struct.PtyKnot.html\" title=\"struct ptyknot::PtyKnot\">PtyKnot</a>",];
+implementors["ptyknot"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"ptyknot/struct.PtyKnot.html\" title=\"struct ptyknot::PtyKnot\">PtyKnot</a>",synthetic:false,types:["ptyknot::PtyKnot"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
